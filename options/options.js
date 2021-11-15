49f6488f03bcb07e108aae3f142ffd6c6e40784f
Crowdbotics <team@crowdbotics.com>
2021-11-15 19:41:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "wandering_bread_32002",
+  url: "https://wandering_bread_32002.botics.co",
+  api: "https://wandering_bread_32002.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
